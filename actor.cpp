3a7709e03333988f1673b4205eea91f1b3857eda
pokpoongryu <pokpoongryu@gmail.com>
2020-12-12 11:08:08 +0900

    actor class

---
+++ actor.cpp
@@ -0,0 +1,8 @@
+#include "actor.h"
+#include <iostream.h>
+using namespace std;
+
+void CActor::Doit()
+{
+	cout<<"CActor::Doit"<<endl;
+}
---
+++ actor.h
@@ -0,0 +1,5 @@
+class CActor
+{
+public:
+	void Doit();
+};
